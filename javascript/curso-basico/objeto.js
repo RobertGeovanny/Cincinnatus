47f40275a7fa87f013c32be1ee403e5d8ef4e860
Robert Geovanny <robertvasquez2050@gmail.com>
2025-05-23 09:22:00 -0400

    Eliminando archivos

--- javascript/curso-basico/objeto.js
+++
@@ -1,8 +0,0 @@
-// para crear objetos se recomienda crearlos con const
-const auto = {
-    marca: "toyota",
-    modelo: 2023,
-    color: "azul",
-    accesorios: ['motor', 'llantas', 'luces', 'aleron']
-};
-console.log(auto.marca); // para extraer algun dato en especifico coloca el nombre del objeto mas un '.'
--- javascript/curso-basico/tipos_datos.js
+++
@@ -1,3 +0,0 @@
-var tipo;
-tipo = 123;
-console.log(typeof tipo) // typeof sirve para saber el tipo de dato que guarda una variable
