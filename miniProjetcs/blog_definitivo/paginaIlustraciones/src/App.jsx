18fc25c09f7ecf6db1cd62967f11d73abacc67a1
Robert Geovanny <robertvasquez2050@gmail.com>
2025-06-03 11:58:11 -0400

    Iniciando proyecto del Blog Definitivo con React y JavaScript

---
+++ miniProjetcs/blog_definitivo/paginaIlustraciones/src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+
+
+function App() {
+  
+  return (
+    <>
+      <header className='cabezera'>
+        <h1 className='Titulo'>Bienvenido a mi blog de Ilustraciones.</h1>
+        <p className=''>En este blog de ilustraciones encontraras las que para mi son las mejores ilustraciones de videojuegos reconocidos ampliamente por la comunidad Gamer.</p>
+      </header>
+      <main className='contenedor'>
+        <section className='contenedor-principal '>
+          
+        </section>
+      </main>
+      <footer className='footer'></footer>
+    </>
+  )
+}
+
+export default App
